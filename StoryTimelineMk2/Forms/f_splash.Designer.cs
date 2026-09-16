3d94e56bf910a7567806df22cfb17d14de9ef1dc
WolfyD <wolfyden@gmail.com>
2026-09-16 23:00:45 +0200

    Added temporary title and stuff

--- StoryTimelineMk2/Forms/f_splash.Designer.cs
+++ StoryTimelineMk2/Forms/f_splash.Designer.cs
@@ -28,7 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = Properties.Resources.title;
+            pictureBox1.Location = new Point(8, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(480, 115);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 48F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(394, 93);
+            label1.Name = "label1";
+            label1.Size = new Size(103, 86);
+            label1.TabIndex = 1;
+            label1.Text = "v2";
             // 
             // f_splash
             // 
@@ -36,13 +62,20 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(15, 23, 42);
             ClientSize = new Size(500, 600);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "f_splash";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "f_splash";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
     }
 }
